00c6432961dd1fbf1e7e0e5208a33c4c68a27eda
Aaron Emery <aemery14@gmail.com>
2015-09-08 17:57:00 -0400

    Removed the period on money

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ var swords = Number(document.querySelector('#swords').value);
 var totalCost = thimbles + (swords * 37);
 
 if (totalCost === 1) {
-  document.querySelector("#the-total").textContent = totalCost + " money.";
+  document.querySelector("#the-total").textContent = totalCost + " money";
 } else {
-  document.querySelector("#the-total").textContent = totalCost + " monies.";
+  document.querySelector("#the-total").textContent = totalCost + " monies";
 }
 
 });
