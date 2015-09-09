477ff438228b422b854688168161fd7b82186e3d
Aaron Emery <aemery14@gmail.com>
2015-09-09 09:14:02 -0400

    Shortened code to one variable

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 document.querySelector("#update-button").addEventListener('click', function() {
 
-var thimbles = Number(document.querySelector('#thimbles').value);
-var swords = Number(document.querySelector('#swords').value);
-
-var totalCost = thimbles + (swords * 37);
+var totalCost = Number(document.querySelector('#thimbles').value) + (Number(document.querySelector('#swords').value) * 37);
 
 if (totalCost === 1) {
   document.querySelector("#the-total").textContent = totalCost + " money";
